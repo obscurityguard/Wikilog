502dd13fcfd9828a02842fd168fb9311ba69e900
Amanda Hildebrandt <amandasouthbye@gmail.com>
2018-02-25 17:02:54 +1100

    Added Wikipedia URL test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Wikilog
 Wikilog is a Chrome extension for tracking Wikipedia history.
+
+To Do:
+1. Find the URL of the current page. Done.
+2. Check if it's a Wikipedia page. Done.
+3. Store the URL.
--- eventPage.js
+++ eventPage.js
@@ -2,5 +2,8 @@
 chrome.tabs.onActivated.addListener(function (tabInfo){
   chrome.tabs.get(tabInfo.tabId, function (tab){
     console.log(tab)
+    const wikipediaRegex = new RegExp("https:\/\/..\.wikipedia\.org\/wiki\/.+")
+    let match = wikipediaRegex.test(tab.url)
+    console.log(match)
   })
 })
