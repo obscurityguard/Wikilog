ca5c9084a8d9af2cc7185b77c73835bcfbe3efff
Amanda Hildebrandt <amandasouthbye@gmail.com>
2018-03-17 21:01:01 +1100

    Showed urls in the popup

--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab){
-  //console.log(tabId, changeInfo, tab)
-  alert("Hello")
+var urls = localStorage.getItem("urls")
+urls = JSON.parse(urls)
+//alert(document.getElementById("container").innerHTML)
+document.getElementById("container").innerHTML = urls.map(function(entry){
+  var date = entry.date
+  date = new Date(date)
+  return `<li>${date.toLocaleString('en-AU')} - ${entry.url}</li>`
 })
