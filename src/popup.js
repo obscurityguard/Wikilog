b3a610a255079d3c57258f06750a49e5d31c54d2
Amanda Hildebrandt <amandasouthbye@gmail.com>
2018-05-06 11:42:52 +1000

    setup React rendering.

---
+++ src/popup.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+class HistoryList extends React.Component {
+
+  render(){
+    return <b>Reacccccct</b>
+
+
+  }
+}
+
+
+var element = document.getElementById("container")
+ReactDOM.render(<HistoryList/>, element)
